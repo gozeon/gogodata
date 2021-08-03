3172032f245651d044363e18d426a62db30c9602
goze <goze.qiu@gmail.com>
2021-08-03 16:37:49 +0800

    chore: init search

--- group/find_all.go
+++ group/find_all.go
@@ -4,14 +4,19 @@ import (
 	"gogodata/conf"
 	"gogodata/model"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
 func DoFindAll(c *gin.Context) {
 	var json []model.Group
-
-	result := conf.DB.Find(&json)
+	search := c.DefaultQuery("search", "")
+	var searchLike strings.Builder
+	searchLike.WriteString("%")
+	searchLike.WriteString(search)
+	searchLike.WriteString("%")
+	result := conf.DB.Where("name LIKE ?", searchLike.String()).Or("description LIKE ?", searchLike.String()).Find(&json)
 	if result.Error != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"message": result.Error.Error()})
 		return
